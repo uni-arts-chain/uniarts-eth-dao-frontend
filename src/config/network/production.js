de58e691232e2ea1c679a0d04c594b2c5838fe30
Lay Hunt <tianxiemaochiyu@outlook.com>
2021-09-22 14:10:22 +0800

    update config

--- src/config/network/production.js
+++ src/config/network/production.js
@@ -1,12 +1,12 @@
 "use strict";
 module.exports = {
   contracts: {
-    TokenLocker: "0x9aE4B33676A1E4f02FeC7B868e73C38C2f5Cf2f0",
-    VoteMining: "0xB332C8a54fCBAd67Da24828BB129E1B05AFf831A",
+    TokenLocker: "0x59eD992BC4403FAF2a83C0D9E2213DF4d8d8Eb86",
+    VoteMining: "0x4f05269BA9a72cB73fc0d351430b0AbA34507D90",
   },
   nfts: {
     UniartsNFT: {
-      address: "0x4c195e61416702F678b1ACEe72f36A5986c205df",
+      address: "0x5F48872D2F81d555c287F1dc2379B6E5fe1346C8",
       symbol: "UANFT",
     },
   },
@@ -14,12 +14,12 @@ module.exports = {
     UART: {
       symbol: "UART",
       decimals: 12,
-      address: "0x3E62aC82c9C3c7525A7Ec10b254C1ee1b1695996",
+      address: "0xf51D9714b74644a224E7f62E56D4A87e1f1fb2E0",
     },
     USDT: {
       symbol: "USDT",
       decimals: 18,
-      address: "0x5F27080e25aaB76e0e3A7Ae0189486F461951F37",
+      address: "0xFd9CA9e22bd54260B87834966d26055620aFdbFf",
     },
   },
 };
