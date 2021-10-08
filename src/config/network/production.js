82f79abae2b340aa4f23fd8462c4aeb8f5e73eb3
bari <br45604560>
2021-10-08 18:15:49 +0800

    新增TrustMarketplace合约

--- src/config/network/production.js
+++ src/config/network/production.js
@@ -4,6 +4,7 @@ module.exports = {
     TokenLocker: "0x59eD992BC4403FAF2a83C0D9E2213DF4d8d8Eb86",
     VoteMining: "0xd9ed8F4d1F66Ae933E4ea3c752886d8Ab392D276",
     Auction: "0xe473ccEbF3FBfEA26da9129958f48f39dbA5F165",
+    TrustMarketplace: "0x88018509F438422E3254A3c50227Ad54C9E8d4D3",
   },
   nfts: {
     UniartsNFT: {
---
+++ src/contracts/TrustMarketplace.js
@@ -0,0 +1,34 @@
+import Web3 from "web3";
+import Wallet from "@/plugins/wallet";
+import { DAPP_CONFIG } from "@/config";
+import TrustMarketplaceABI from "@/contracts/abi/TrustMarketplace.json";
+import store from "@/store";
+
+class TrustMarketplace {
+  constructor() {
+    this.web3 = new Web3(Wallet.provider);
+    this.address = DAPP_CONFIG.contracts.TrustMarketplace;
+    // 初始化合约
+    this.contract = new this.web3.eth.Contract(TrustMarketplaceABI, this.address.toString());
+  }
+  // buyNow
+  async creatOrder(nftAddress, assetId, priceInWei, expiresAt, callback) {
+    const sender = store.state.user.info.address;
+    const gasPrice = await this.gasPrice();
+    const tx = this.contract.createOrder(nftAddress, assetId, priceInWei, expiresAt);
+    const gasLimit = await tx.estimateGas({
+      value: 0,
+      from: sender,
+      to: this.address,
+    });
+    return tx.send(
+      {
+        from: sender,
+        gasPrice: gasPrice,
+        gas: Math.round(gasLimit * 1.1),
+      },
+      callback
+    );
+  }
+}
+export default new TrustMarketplace();
