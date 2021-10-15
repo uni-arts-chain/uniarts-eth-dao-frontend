4d14a1acecbfaa739d06188474152418def3aa33
bari <br45604560>
2021-10-15 19:22:55 +0800

    BASE_URL

--- src/config/index.js
+++ src/config/index.js
@@ -2,7 +2,7 @@ import DappConfig from "./network";
 
 export const NODE_ENV = process.env.NODE_ENV || "development";
 
-export const BASE_URL = "https://dapp.uniarts.network";
+export const BASE_URL = "https://af.uniarts.network";
 
 export const PREFIX = "/api/v1";
 
@@ -10,8 +10,8 @@ export const PREFIX = "/api/v1";
 export const HTTP_DEFAULT_CONFIG = {
   prefix: PREFIX,
   baseURL: NODE_ENV === "production" ? BASE_URL : "https://dapp.uniarts.network", // baseUrl
-  isProd: NODE_ENV === "production" ? true : true, // false：访问测试接口  true: 访问线上接口
-  debug: NODE_ENV === "production" ? false : false,
+  isProd: NODE_ENV === "production", // false：访问测试接口  true: 访问线上接口
+  debug: NODE_ENV === "production",
 };
 
 export const CHAIN_DEFAULT_CONFIG = window.NODE_CONFIG || {};
