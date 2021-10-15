b7ed6317ff427755b685b6fed86b021344a6a6a8
Lay Hunt <tianxiemaochiyu@outlook.com>
2021-10-15 22:35:50 +0800

    config update

--- src/config/index.js
+++ src/config/index.js
@@ -6,12 +6,16 @@ export const BASE_URL = "https://af.uniarts.network";
 
 export const PREFIX = "/api/v1";
 
+export const isProd = NODE_ENV === "production" ? true : false; //条件非真时 false：访问测试接口, true: 访问线上接口
+
 // HTTP_API 默认配置
 export const HTTP_DEFAULT_CONFIG = {
   prefix: PREFIX,
-  baseURL: NODE_ENV === "production" ? BASE_URL : "https://dapp.uniarts.network", // baseUrl
-  isProd: NODE_ENV === "production", // false：访问测试接口  true: 访问线上接口
-  debug: NODE_ENV === "production",
+  // 开发环境下模拟生产环境，方便调试
+  baseURL:
+    NODE_ENV === "production" ? BASE_URL : isProd ? BASE_URL : "https://dapp.uniarts.network", // baseUrl
+  isProd: isProd,
+  debug: NODE_ENV !== "production",
 };
 
 export const CHAIN_DEFAULT_CONFIG = window.NODE_CONFIG || {};
