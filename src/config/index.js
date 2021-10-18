ddc139938710a25ffe1f89f43ea496447ca560b7
Lay Hunt <tianxiemaochiyu@outlook.com>
2021-10-18 15:21:00 +0800

    update

--- src/config/index.js
+++ src/config/index.js
@@ -2,18 +2,17 @@ import DappConfig from "./network";
 
 export const NODE_ENV = process.env.NODE_ENV || "development";
 
-export const BASE_URL = "https://af.uniarts.network";
+// 开发环境下模拟生产环境，方便调试
+export const isProd = NODE_ENV === "production" ? true : true; //条件非真时 false：访问测试接口, true: 访问线上接口
+
+export const BASE_URL = isProd ? "https://af.uniarts.network" : "https://dapp.uniarts.network";
 
 export const PREFIX = "/api/v1";
-
-export const isProd = NODE_ENV === "production" ? true : true; //条件非真时 false：访问测试接口, true: 访问线上接口
 
 // HTTP_API 默认配置
 export const HTTP_DEFAULT_CONFIG = {
   prefix: PREFIX,
-  // 开发环境下模拟生产环境，方便调试
-  baseURL:
-    NODE_ENV === "production" ? BASE_URL : isProd ? BASE_URL : "https://dapp.uniarts.network", // baseUrl
+  baseURL: BASE_URL,
   isProd: isProd,
   debug: NODE_ENV !== "production",
 };
