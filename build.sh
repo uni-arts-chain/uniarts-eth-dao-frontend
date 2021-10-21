215bad74936d5da42ceed081956848152a01a45c
lixiumiao <lixiumiao@gmail.com>
2021-10-21 09:41:24 +0800

    fixbug

--- build.sh
+++ build.sh
@@ -1,6 +1,8 @@
 #!/bin/sh
 
 docker build -t uni-arts-airdrop .
-docker tag uni-arts-airdrop registry.cn-hongkong.aliyuncs.com/tatmas/uni-arts-airdrop
-docker login --username=lixiumiao@1837074167842277 registry.cn-hongkong.aliyuncs.com
-docker push registry.cn-hongkong.aliyuncs.com/tatmas/uni-arts-airdrop
+docker tag uni-arts-airdrop registry.cn-hongkong.aliyuncs.com/uniarts/uni-arts-airdrop
+docker login --username=jieliantech --password=tatmas2020 registry.cn-hongkong.aliyuncs.com
+docker push registry.cn-hongkong.aliyuncs.com/uniarts/uni-arts-airdrop
+docker tag uni-arts-airdrop registry.cn-hongkong.aliyuncs.com/uniarts/uni-arts-airdrop:v1.0.0
+docker push registry.cn-hongkong.aliyuncs.com/uniarts/uni-arts-airdrop:v1.0.0
