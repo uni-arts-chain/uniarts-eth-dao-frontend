4917abd1f54a6bd15d79739ce2b7dc8cc5619c89
lixiumiao <lixiumiao@gmail.com>
2021-10-29 18:29:52 +0800

    test env

--- build.sh
+++ build.sh
@@ -1,8 +1,6 @@
 #!/bin/sh
 
-docker build -t uni-arts-airdrop .
-docker tag uni-arts-airdrop registry.cn-hongkong.aliyuncs.com/uniarts/uni-arts-airdrop
-docker login --username=jieliantech --password=tatmas2020 registry.cn-hongkong.aliyuncs.com
-docker push registry.cn-hongkong.aliyuncs.com/uniarts/uni-arts-airdrop
-docker tag uni-arts-airdrop registry.cn-hongkong.aliyuncs.com/uniarts/uni-arts-airdrop:v1.0.0
-docker push registry.cn-hongkong.aliyuncs.com/uniarts/uni-arts-airdrop:v1.0.0
+docker build -t uni-arts-airdrop-demo .
+docker tag uni-arts-airdrop-demo registry.cn-hongkong.aliyuncs.com/tatmas/uni-arts-airdrop:demo
+docker login --username=lixiumiao@1837074167842277 --password=tatmas2020 registry.cn-hongkong.aliyuncs.com
+docker push registry.cn-hongkong.aliyuncs.com/tatmas/uni-arts-airdrop:demo
