dd89c7cdf9a462dd82f95749772391e57229bb24
bari <br45604560>
2021-11-11 17:20:18 +0800

    src/config/network/production.js

--- src/config/network/production.js
+++ src/config/network/production.js
@@ -2,10 +2,11 @@
 module.exports = {
   contracts: {
     TokenLocker: "0x507BDe03A87a6aA134d16634545E3D79c11c137D",
-    VoteMining: "0x9F98B18d56Af67409c36C4c23D349ea3B5035c05",
+    VoteMiningV1: "0x9F98B18d56Af67409c36C4c23D349ea3B5035c05",
+    VoteMining: "0x4FA3DECEFcd83EdAe0828FdE49221C448961016b",
     Auction: "0xCE7c240079CC64bA531612dADfD086841427855C",
-    TrustMarketplace: "0x144D738944Ea127479b3E79fB90C8ccF4E98093C",
-    Collection: "0x1E85cFea30d167Df9c596EbaC183e14dc506cf61",
+    TrustMarketplace: "0xCe37caCC1c616c6736Da974116b37102C3431f21",
+    Collection: "0xb4873626519754185FF5a8A368D442230645e524",
   },
   nfts: {
     UniartsNFT: {
@@ -19,23 +20,23 @@ module.exports = {
       decimals: 18,
       address: "0x7ceb23fd6bc0add59e62ac25578270cff1b9f619",
     },
-    USDT: {
-      symbol: "USDT",
-      decimals: 6,
-      address: "0xc2132D05D31c914a87C6611C10748AEb04B58e8F",
-    },
+    // USDT: {
+    //   symbol: "USDT",
+    //   decimals: 6,
+    //   address: "0xc2132D05D31c914a87C6611C10748AEb04B58e8F",
+    // },
   },
   tokens: {
     UART: {
       symbol: "UART",
       decimals: 12,
-      address: "0xf244E91A46A9cdd48da295cA5d0B27894f8032B1",
+      address: "0x3c2915050347dCb122Aa5F7A148a5eED29Cccfa8",
     },
-    USDT: {
-      symbol: "USDT",
-      decimals: 6,
-      address: "0xc2132D05D31c914a87C6611C10748AEb04B58e8F",
-    },
+    // USDT: {
+    //   symbol: "USDT",
+    //   decimals: 6,
+    //   address: "0xc2132D05D31c914a87C6611C10748AEb04B58e8F",
+    // },
     WETH: {
       symbol: "wETH",
       decimals: 18,
