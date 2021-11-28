c67f4e591985974161413d64205406de4d9b45fd
Lay Hunt <tianxiemaochiyu@outlook.com>
2021-11-28 20:51:08 +0800

    fixed some bug

--- src/config/networkConfig.js
+++ src/config/networkConfig.js
@@ -1,6 +1,6 @@
 export default {
-  development: "https://dapp.uniarts.network",
-  production: "https://af.uniarts.network",
-  networkDevelopment: "development",
-  networkProduction: "production",
+  development: "https://af.uniarts.network",
+  production: "https://dapp.uniarts.network",
+  networkDevelopment: "production",
+  networkProduction: "development",
 };
