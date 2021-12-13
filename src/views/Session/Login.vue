8bbf1a0d5a101acaf2e38e1c9ecb0e12a88a94c7
Lay Hunt <tianxiemaochiyu@outlook.com>
2021-12-13 19:03:56 +0800

    add signup dialog

--- src/views/Session/Login.vue
+++ src/views/Session/Login.vue
@@ -5,6 +5,54 @@
       <img src="@/assets/images/banner-logo@2x.png" />
       <p>Welcome ! Please sign in</p>
       <button v-loading="isLoading" @click="onConnectMetaMask">Sign in</button>
+      <div class="support-wallet-info" v-if="!$store.state.global.isMobile">
+        <span class="info-title">Connect with one of our available wallet providers </span>
+        <div class="wallet-icon">
+          <a
+            href="https://chrome.google.com/webstore/detail/nkbihfbeogaeaoehlefnkodbefgpgknn"
+            target="_blank"
+            class="wallet-item"
+          >
+            <icon-svg icon-class="metamask" />
+          </a>
+          <a
+            href="https://chrome.google.com/webstore/detail/bitkeep/jiidiaalihmmhddjgbnbgdfflelocpak"
+            target="_blank"
+            class="wallet-item"
+          >
+            <icon-svg icon-class="bitkeep" style="font-size: 77px" />
+          </a>
+        </div>
+      </div>
+      <Dialog
+        v-model="dialogTableVisible"
+        customClass="retrieve-dialog"
+        v-if="!$store.state.global.isMobile"
+        type="small"
+        width="380px"
+      >
+        <div class="dialog-content">
+          <div class="dialog-title">You need an Ethereum wallet to use Art Formula.</div>
+          <div class="wallet-list">
+            <a
+              class="wallet"
+              href="https://chrome.google.com/webstore/detail/nkbihfbeogaeaoehlefnkodbefgpgknn"
+              target="_blank"
+            >
+              <icon-svg style="font-size: 45px" icon-class="metamask" />
+              <span>MetaMask</span>
+            </a>
+            <a
+              class="wallet"
+              href="https://chrome.google.com/webstore/detail/bitkeep/jiidiaalihmmhddjgbnbgdfflelocpak"
+              target="_blank"
+            >
+              <icon-svg style="font-size: 44px" icon-class="bitkeep" />
+              <span>BitKeep</span>
+            </a>
+          </div>
+        </div>
+      </Dialog>
     </div>
     <div v-if="!isNeedSignUp && isChecked" class="sign-in">
       <p>Welcome back</p>
@@ -51,9 +99,13 @@ import { notification } from "@/components/Notification";
 import http from "@/plugins/http";
 import wallet from "@/plugins/wallet";
 import store from "@/store";
+import Dialog from "@/components/Dialog";
 
 export default defineComponent({
   name: "login",
+  components: {
+    Dialog,
+  },
   setup() {
     // TODO
     const router = useRouter();
@@ -153,7 +205,11 @@ export default defineComponent({
           console.log(err);
           isLoading.value = false;
           if (err.code === 100) {
-            notification.error("Please install the selected wallet");
+            if (!store.state.global.isMobile) {
+              dialogTableVisible.value = true;
+            } else {
+              notification.error("Please install the selected wallet");
+            }
           } else if (err.head && err.head.code === 6010) {
             isNeedSignUp.value = true;
             isChecked.value = true;
@@ -162,6 +218,8 @@ export default defineComponent({
           }
         });
     };
+
+    const dialogTableVisible = ref(false);
 
     return {
       isLoading,
@@ -173,6 +231,8 @@ export default defineComponent({
       address,
       userIntro,
       registerForm,
+
+      dialogTableVisible,
     };
   },
   beforeRouteEnter(to, from, next) {
@@ -223,6 +283,32 @@ export default defineComponent({
     color: #ffffff;
     line-height: 34px;
     cursor: pointer;
+  }
+
+  .support-wallet-info {
+    margin-top: 100px;
+    .info-title {
+      font-weight: 400;
+      font-size: 17px;
+      color: #909399;
+    }
+    .wallet-icon {
+      display: flex;
+      justify-content: center;
+      align-items: center;
+      margin-top: 40px;
+      .wallet-item {
+        width: 90px;
+        height: 90px;
+        display: flex;
+        align-items: center;
+        justify-content: center;
+        margin: 0 60px;
+      }
+      .wallet-item .svg-icon {
+        font-size: 90px;
+      }
+    }
   }
 }
 
@@ -322,6 +408,41 @@ export default defineComponent({
     background: black;
     color: white;
     cursor: pointer;
+  }
+}
+
+.dialog-content {
+  padding: 40px 30px;
+  .dialog-title {
+    text-align: center;
+    font-weight: 900;
+    font-size: 20px;
+    color: rgb(4, 17, 29);
+    line-height: 110%;
+    margin-bottom: 34px;
+    word-break: break-word;
+  }
+  .wallet-list {
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    margin: 40px 0 20px 0;
+    .wallet {
+      width: 100%;
+      margin: 10px 0;
+      border: 2px solid black;
+      border-radius: 6px;
+      display: flex;
+      justify-content: center;
+      align-items: center;
+      padding: 5px 30px;
+    }
+    .wallet > span {
+      margin-left: 20px;
+      font-size: 20px;
+      display: block;
+      width: 120px;
+    }
   }
 }
 
