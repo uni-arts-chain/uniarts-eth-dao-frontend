4e2468ec7184507630b786b936b1cee0c59b865a
bari <br45604560>
2021-12-14 17:48:22 +0800

    Revert "fixed some bug"
    This reverts commit c67f4e59

--- src/config/networkConfig.js
+++ src/config/networkConfig.js
@@ -1,6 +1,6 @@
 export default {
-  development: "https://dapp.uniarts.network" || "https://af.uniarts.network",
-  production: "https://dapp.uniarts.network",
-  networkDevelopment: "development" || "production",
-  networkProduction: "development",
+  development: "https://dapp.uniarts.network",
+  production: "https://af.uniarts.network",
+  networkDevelopment: "development",
+  networkProduction: "production",
 };
