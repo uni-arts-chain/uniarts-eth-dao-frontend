907edadeda51c712cbcd2f18e0bd5badaefb1d18
Lay Hunt <tianxiemaochiyu@outlook.com>
2022-01-17 11:37:30 +0800

    add build no-cache

--- build.sh
+++ build.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-docker build -t uni-arts-airdrop .
+docker build -t uni-arts-airdrop --no-cache .
 docker tag uni-arts-airdrop registry.cn-hongkong.aliyuncs.com/uniarts/uni-arts-airdrop
 docker login --username=jieliantech --password=tatmas2020 registry.cn-hongkong.aliyuncs.com
 docker push registry.cn-hongkong.aliyuncs.com/uniarts/uni-arts-airdrop
